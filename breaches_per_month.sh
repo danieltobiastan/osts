b47d6c86453c6f228b1a478f66b491a8077e371e
Daniel Tobias Tan <22684196@student.uwa.edu.au>
2023-05-16 17:42:10 +0800

    Added Data Analysis

--- breaches_per_month.sh
+++ breaches_per_month.sh
@@ -35,5 +35,15 @@ else
 		{print a[(NR+1)/2]} 
 		else 
 		{print (a[NR/2] + a[NR/2+1])/2}}')
-	echo "Median=$median, MAD=$mad"
+
+	# main program to output months (create array and upper lower median bounds)
+	main=$(echo "$sorted_months" | awk -v median=$median -v mad=$mad 'BEGIN{months[1]="Jan"; months[2]="Feb"; months[3]="Mar";months[4]="Apr";months[5]="May";months[6]="Jun";months[7]="Jul";months[8]="Aug";months[9]="Sep";months[10]="Oct";months[11]="Nov";months[12]="Dec";upper_median=median+mad; lower_median=median-mad}
+
+{if ($1 > upper_median)
+	{print months[$2]"\t"$1"\t""++"}
+else if ($1 < lower_median)
+	{print months[$2]"\t"$1"\t""--"}
+else 
+	{print months[$2]"\t"$1}}')	
+	echo "$main"
 fi
